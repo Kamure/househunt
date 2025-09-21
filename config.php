cff5d9249acaa5361533aa3516c15de4d7d1de56
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-09-21 21:51:45 +0300

    Configuration

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+$conf = [
+    'site_name'   => 'Househunt',
+    'site_email'  => 'support@househunt.com',
+    'site_url'    => 'http://localhost/househunt',
+    'language'    => 'en',
+
+    
+    'db_type'     => 'mysqli', 
+    'db_host'     => 'localhost',
+    'db_user'     => 'root',
+    'db_pass'     => '',
+    'db_name'     => 'househunt_db',
+
+    
+    'mail_type'   => 'smtp', 
+    'smtp_host'   => 'smtp.gmail.com',
+    'smtp_user'   => 'beyonce.kamure@strathmore.edu',
+    'smtp_pass'   => 'dfbi gwap xclq mwqe', 
+    'smtp_port'   => 465,
+    'smtp_secure' => 'ssl' 
+];
+?>
